719a8e7afb55042ff0f508884b3cde4316bc7f4d
bryann <brags@outlook.es>
2026-07-21 21:15:02 -0500

    upgrade

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ const isProduction = process.env.NODE_ENV === 'production'
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // Necesario para GitHub Pages
-  basePath: isProduction ? '/cv' : undefined, 
-  assetPrefix: isProduction ? '/cv/' : undefined,
+  basePath: isProduction ? '/blog' : undefined, 
+  assetPrefix: isProduction ? '/blog/' : undefined,
   images: {
     unoptimized: true, // Esto soluciona el problema de carga
   },
